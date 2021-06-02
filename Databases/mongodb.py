af7f647b3e510a7d13ce4a94e026411ced9e715b
yash621 <yg17381@gmail.com>
2021-06-02 21:34:55 +0530

    mongo operations

--- Databases/mongodb.py
+++ Databases/mongodb.py
@@ -1,5 +1,13 @@
+from typing import Collection
 import pymongo as go
 client = go.MongoClient(
     "mongodb+srv://yash:yash@cluster0.hiudc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
 print(client.list_database_names())
+db1 = client["jadoo"]
+print(client.list_database_names())
+Collection = db1["test"]
+record = {
+    "jhdj": "jkdjf",
+    "jdkff": "dfjkf"
+}
+Collection.insert_one(record)
